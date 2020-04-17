28e94d4bde82176a734f128987d3113ece593986
Jay Moon <jaymoon9876@gmail.com>
2020-04-17 20:38:43 +0900

    The buttons on MainFragment now navigates to the other fragments

--- app/src/main/java/com/example/learnkorean/MainFragment.java
+++ app/src/main/java/com/example/learnkorean/MainFragment.java
@@ -3,10 +3,12 @@ package com.example.learnkorean;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,9 +22,9 @@ public class MainFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button learnButton;
+    private Button gameButton;
+    private Button helpButton;
 
     public MainFragment() {
         // Required empty public constructor
@@ -49,16 +51,46 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // TODO: Decide if the if-statement below is needed
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get reference to the buttons
+        learnButton = view.findViewById(R.id.learn_button);
+        gameButton = view.findViewById(R.id.game_button);
+        helpButton = view.findViewById(R.id.help_button);
+
+        // Set listener to the buttons
+        learnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.learnFragmentAction);
+            }
+        });
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.gameFragmentAction);
+            }
+        });
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.helpFragmentAction);
+            }
+        });
+
+        return view;
     }
 }
