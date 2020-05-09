1d4ecb26b34595dd9d6e925725456d67b9a21fc7
Jay Moon <jaymoon9876@gmail.com>
2020-05-09 22:11:00 +0900

    Button texts in the learn fragment is now randomly generated

--- app/src/main/java/com/example/learnkorean/Character.java
+++ app/src/main/java/com/example/learnkorean/Character.java
@@ -2,12 +2,24 @@ package com.example.learnkorean;
 
 public class Character {
 
+    private int id;
     private String korean;
     private String english;
 
-    public Character(String iKorean, String iEnglish) {
+    public Character(int iId, String iKorean, String iEnglish) {
+        id = iId;
         korean = iKorean;
         english = iEnglish;
+    }
+
+    public Character(String iKorean, String iEnglish) {
+        id = -1;
+        korean = iKorean;
+        english = iEnglish;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getKorean() {
--- app/src/main/java/com/example/learnkorean/LearnFragment.java
+++ app/src/main/java/com/example/learnkorean/LearnFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 
 /**
@@ -22,7 +25,8 @@ import java.util.ArrayList;
  */
 public class LearnFragment extends Fragment {
     // Global variables
-    private ArrayList<Character> charList;
+    private ArrayList<Character> characters;
+    private ArrayList<Integer> randomNumbers;
 
     private Button button1;
     private Button button2;
@@ -59,24 +63,60 @@ public class LearnFragment extends Fragment {
         // Required empty public constructor
     }
 
-    private void loadDataset(int resourceId) {
+    /**
+     * Method to load dataset to characters list
+     *
+     * @param resourceId type int, id of a dataset resource
+     * @return type int, size of a dataset
+     */
+    private int loadDataset(int resourceId) {
         InputStream is = this.getResources().openRawResource(resourceId);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String line1 = null;
-        String line2 = null;
+        int datasetSize = 0;
+        String korean = null;
+        String english = null;
 
         try {
+            // Read the first line that contains the size of the dataset
+            datasetSize = Integer.parseInt(br.readLine());
+
             // While the next line is not null
-            while ((null != (line1 = br.readLine())) && (null != (line2 = br.readLine()))) {
-                charList.add(new Character(line1, line2));
+            while ((null != (korean = br.readLine())) && (null != (english = br.readLine()))) {
+                characters.add(new Character(korean, english));
             }
 
             is.close();
             br.close();
         }
-        catch (IOException e) {
+        catch (NumberFormatException | IOException e) {
             e.printStackTrace();
         }
+
+        return datasetSize;
+    }
+
+    /**
+     * Method to generate random numbers to randomNumbers list
+     *
+     * @param max type int, maximum range (highest possible number) of random numbers
+     */
+    private void generateRandomNumbers(int max) {
+        Random random = new Random();
+        int counter = 0;            // Incremented from zero to max
+        int numerator = 12;         // Decremented from 12 to zero
+        int denominator = max;      // Decremented from max
+
+        while(counter < max && numerator > 0) {
+            if (random.nextDouble() < (double) numerator / denominator) {
+                randomNumbers.add(counter);
+                numerator--;
+            }
+            counter++;
+            denominator--;
+        }
+
+        // Shuffle list randomly
+        Collections.shuffle(randomNumbers);
     }
 
     @Override
@@ -84,7 +124,8 @@ public class LearnFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         // Initialize global variables on creating this fragment
-        charList = new ArrayList<Character>();
+        characters = new ArrayList<Character>();
+        randomNumbers = new ArrayList<Integer>(12);
 
         button1Clicked = false;
         button2Clicked = false;
@@ -100,7 +141,10 @@ public class LearnFragment extends Fragment {
         button12Clicked = false;
 
         // Load character list from the dataset
-        loadDataset(R.raw.dataset);
+        int datasetSize = loadDataset(R.raw.dataset);
+
+        // Generate random numbers
+        generateRandomNumbers(datasetSize);
     }
 
     @Override
@@ -130,31 +174,31 @@ public class LearnFragment extends Fragment {
         unclickedButtonTextColor = ResourcesCompat.getColor(getResources(), R.color.colorLearnButtonTextUnclicked, null);
 
         // Set button text
-        button1.setText(charList.get(0).getKorean());
-        button2.setText(R.string.unclick);
-        button3.setText(R.string.unclick);
-        button4.setText(R.string.unclick);
-        button5.setText(R.string.unclick);
-        button6.setText(R.string.unclick);
-        button7.setText(R.string.unclick);
-        button8.setText(R.string.unclick);
-        button9.setText(R.string.unclick);
-        button10.setText(R.string.unclick);
-        button11.setText(R.string.unclick);
-        button12.setText(R.string.unclick);
+        button1.setText(characters.get(randomNumbers.get(0)).getKorean());
+        button2.setText(characters.get(randomNumbers.get(1)).getKorean());
+        button3.setText(characters.get(randomNumbers.get(2)).getKorean());
+        button4.setText(characters.get(randomNumbers.get(3)).getKorean());
+        button5.setText(characters.get(randomNumbers.get(4)).getKorean());
+        button6.setText(characters.get(randomNumbers.get(5)).getKorean());
+        button7.setText(characters.get(randomNumbers.get(6)).getKorean());
+        button8.setText(characters.get(randomNumbers.get(7)).getKorean());
+        button9.setText(characters.get(randomNumbers.get(8)).getKorean());
+        button10.setText(characters.get(randomNumbers.get(9)).getKorean());
+        button11.setText(characters.get(randomNumbers.get(10)).getKorean());
+        button12.setText(characters.get(randomNumbers.get(11)).getKorean());
 
         // Set listener to the buttons
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(false == button1Clicked) {
-                    button1.setText(R.string.click);
+                    button1.setText(characters.get(randomNumbers.get(0)).getEnglish());
                     button1.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button1Clicked = true;
                 }
                 else {
-                    button1.setText(R.string.unclick);
+                    button1.setText(characters.get(randomNumbers.get(0)).getKorean());
                     button1.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button1Clicked = false;
@@ -165,13 +209,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button2Clicked) {
-                    button2.setText(R.string.click);
+                    button2.setText(characters.get(randomNumbers.get(1)).getEnglish());
                     button2.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button2Clicked = true;
                 }
                 else {
-                    button2.setText(R.string.unclick);
+                    button2.setText(characters.get(randomNumbers.get(1)).getKorean());
                     button2.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button2Clicked = false;
@@ -182,13 +226,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button3Clicked) {
-                    button3.setText(R.string.click);
+                    button3.setText(characters.get(randomNumbers.get(2)).getEnglish());
                     button3.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button3Clicked = true;
                 }
                 else {
-                    button3.setText(R.string.unclick);
+                    button3.setText(characters.get(randomNumbers.get(2)).getKorean());
                     button3.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button3Clicked = false;
@@ -199,13 +243,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button4Clicked) {
-                    button4.setText(R.string.click);
+                    button4.setText(characters.get(randomNumbers.get(3)).getEnglish());
                     button4.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button4Clicked = true;
                 }
                 else {
-                    button4.setText(R.string.unclick);
+                    button4.setText(characters.get(randomNumbers.get(3)).getKorean());
                     button4.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button4Clicked = false;
@@ -216,13 +260,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button5Clicked) {
-                    button5.setText(R.string.click);
+                    button5.setText(characters.get(randomNumbers.get(4)).getEnglish());
                     button5.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button5Clicked = true;
                 }
                 else {
-                    button5.setText(R.string.unclick);
+                    button5.setText(characters.get(randomNumbers.get(4)).getKorean());
                     button5.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button5Clicked = false;
@@ -233,13 +277,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button6Clicked) {
-                    button6.setText(R.string.click);
+                    button6.setText(characters.get(randomNumbers.get(5)).getEnglish());
                     button6.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button6Clicked = true;
                 }
                 else {
-                    button6.setText(R.string.unclick);
+                    button6.setText(characters.get(randomNumbers.get(5)).getKorean());
                     button6.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button6Clicked = false;
@@ -250,13 +294,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button7Clicked) {
-                    button7.setText(R.string.click);
+                    button7.setText(characters.get(randomNumbers.get(6)).getEnglish());
                     button7.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button7Clicked = true;
                 }
                 else {
-                    button7.setText(R.string.unclick);
+                    button7.setText(characters.get(randomNumbers.get(6)).getKorean());
                     button7.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button7Clicked = false;
@@ -267,13 +311,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button8Clicked) {
-                    button8.setText(R.string.click);
+                    button8.setText(characters.get(randomNumbers.get(7)).getEnglish());
                     button8.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button8Clicked = true;
                 }
                 else {
-                    button8.setText(R.string.unclick);
+                    button8.setText(characters.get(randomNumbers.get(7)).getKorean());
                     button8.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button8Clicked = false;
@@ -284,13 +328,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button9Clicked) {
-                    button9.setText(R.string.click);
+                    button9.setText(characters.get(randomNumbers.get(8)).getEnglish());
                     button9.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button9Clicked = true;
                 }
                 else {
-                    button9.setText(R.string.unclick);
+                    button9.setText(characters.get(randomNumbers.get(8)).getKorean());
                     button9.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button9Clicked = false;
@@ -301,13 +345,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button10Clicked) {
-                    button10.setText(R.string.click);
+                    button10.setText(characters.get(randomNumbers.get(9)).getEnglish());
                     button10.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button10Clicked = true;
                 }
                 else {
-                    button10.setText(R.string.unclick);
+                    button10.setText(characters.get(randomNumbers.get(9)).getKorean());
                     button10.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button10Clicked = false;
@@ -318,13 +362,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button11Clicked) {
-                    button11.setText(R.string.click);
+                    button11.setText(characters.get(randomNumbers.get(10)).getEnglish());
                     button11.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button11Clicked = true;
                 }
                 else {
-                    button11.setText(R.string.unclick);
+                    button11.setText(characters.get(randomNumbers.get(10)).getKorean());
                     button11.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button11Clicked = false;
@@ -335,13 +379,13 @@ public class LearnFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(false == button12Clicked) {
-                    button12.setText(R.string.click);
+                    button12.setText(characters.get(randomNumbers.get(11)).getEnglish());
                     button12.setTextColor(clickedButtonTextColor);
                     view.setBackgroundColor(clickedButtonColor);
                     button12Clicked = true;
                 }
                 else {
-                    button12.setText(R.string.unclick);
+                    button12.setText(characters.get(randomNumbers.get(11)).getKorean());
                     button12.setTextColor(unclickedButtonTextColor);
                     view.setBackgroundColor(unclickedButtonColor);
                     button12Clicked = false;
